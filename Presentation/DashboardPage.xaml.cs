298f084922ae8cf00a67d225f5b29d0bd8741314
Mike Banks <michbanks9@gmail.com>
2018-03-15 17:43:01 -0400

    Added LitView to Dashboard, which loads information from the JSON file

--- Presentation/DashboardPage.xaml.cs
+++ Presentation/DashboardPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using WiFiConnect.BusinessLogic;
 using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -23,45 +25,61 @@ namespace WiFiConnect
     /// </summary>
     public sealed partial class DashboardPage : Page
     {
+
+        private User _user;
+        private Alert[] _alerts;
+
         public DashboardPage()
         {
             this.InitializeComponent();
+
+            //initialized when the page is navigated to
+            _user = null;
+            _alerts = null;
+
+            DisplayAlerts();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             //TODO: Maybe send just first name as a parameter (unless other properties of the user are used)
-            User user = e.Parameter as User;
+            _user = e.Parameter as User;
 
             string day = DateTime.Now.DayOfWeek.ToString();
-            string firstName = user.FirstName;
+            string firstName = _user.FirstName;
 
-            txtWelcome.Text = String.Format("Happy {0} {1}", day, firstName);
-
-            LoadJson();
+            txtWelcome.Text = String.Format("Happy {0} {1}", day, firstName);  
         }
 
-        public async void LoadJson()
+        private async void DisplayAlerts()
+        {
+            await LoadJson();
+
+            lstAlerts.Items.Clear();
+            foreach (Alert alert in _alerts)
+            {
+                lstAlerts.Items.Add(alert);
+            }
+        }
+
+        public async Task LoadJson()
         {
             string url = "https://hanifso.dev.fast.sheridanc.on.ca/Pi/alerts.json";
-
-            string jsonString;
             JsonObject jsonObj = new JsonObject();
+
             using (var httpClient = new System.Net.Http.HttpClient())
             {
-                var stream = await httpClient.GetStreamAsync(url);
+                var stream =  await httpClient.GetStreamAsync(url);
                 StreamReader reader = new StreamReader(stream);
 
                 //stores the entire json file into one object
                 jsonObj = JsonObject.Parse(reader.ReadLine());
-    
             }
 
             //stores all of the alerts into one array
             JsonArray alertArray = jsonObj.GetNamedArray("Alerts");
 
-            //create a string array of the same size as the alert array -----------------------TEMP - maybe?
-            string[] importantAlertInfo = new string[alertArray.Count];
+            _alerts = new Alert[alertArray.Count];
 
             //loops through all of the alerts in the JSON object
             for (uint iAlert = 0; iAlert < alertArray.Count; iAlert++)
@@ -69,25 +87,33 @@ namespace WiFiConnect
                 //create a new JSON Object for each object in the JSON array
                 JsonObject alertObject = alertArray.GetObjectAt(iAlert);
 
-                //extract certain information from each alert
-                importantAlertInfo[iAlert] = String.Format("Alert ID: {0} Flowerpot ID: {1} Alert Level: {2}", alertObject.GetNamedString("Alert ID"),
-                    alertObject.GetNamedString("Flowerpot ID"), alertObject.GetNamedString("Level"));
-            }
-            //gets the first alert
+                //extract information from each alert
+                int alertID = Int32.Parse(alertObject.GetNamedString("Alert ID"));
+                string flowerpotID = alertObject.GetNamedString("Flowerpot ID");
+                DateTime alertDateTime = DateTime.Parse(alertObject.GetNamedString("Alert Timestamp"));
+                string shortDescription = alertObject.GetNamedString("Short Description");
+                string longDescription = alertObject.GetNamedString("Long Description");
+                //TODO: GET ACKNOWLEDGE DATE TIME - not a valid date right now
+                //DateTime acknowledgeDateTime = DateTime.Parse(alertObject.GetNamedString("Acknowledged Timestamp"));
+                int alertLevel = Int32.Parse(alertObject.GetNamedString("Level"));
+                //TODO: GET IMAGE - set to null right now
+                //TODO: GET SOUND - set to null right now
 
-            //gets the first alert from the first alertObject
-            txtNoTags.Text = "";
-            for (int i = 0; i < importantAlertInfo.Count(); i++)
-            {
-                txtNoTags.Text += String.Format("-----{0}-----", i);
-                txtNoTags.Text += importantAlertInfo[i];
+                //add the alert into an array of alerts
+                //TODO: Temporarily adding the alert date time in place of the acknowledge date time
+                Alert alert = new Alert(alertID, flowerpotID, alertDateTime, shortDescription, longDescription, alertDateTime, alertLevel);
+                _alerts[iAlert] = alert;
             }
-            
          }
 
         private void OnTagSetupClick(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(TagSetupPage));
+        }
+
+        private void OnAlertSelected(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
